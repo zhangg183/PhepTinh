ad658aeb14b68d51fa0342f8396718ad54f16424
zhangg183 <phanhieu183203@gmail.com>
2025-04-28 20:39:15 +0700

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,7 +1,10 @@
 #include<bits/stdc++.h> 
 using namespace std;
-
+int tong(int a,int b, int c){
+	return a+b+c;
+}
 int main(){
 	int a=7,b=8,c=9;
-	
+	int Tong = tong(a,b,c);
+	cout<<Tong<<endl;
 }
