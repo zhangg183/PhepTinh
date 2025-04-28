f5725f5007517eb9da961f54852c99994303135a
zhangg183 <phanhieu183203@gmail.com>
2025-04-28 20:57:26 +0700

    Update SoThuc.cpp

--- SoThuc.cpp
+++ SoThuc.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main(){
 	float a=1.2,b=2.3,c=3.4;
-
+        float Tong = tong(a,b,c);
+	float Hieu = hieu(a,b,c);
+	float Nhan = tich(a,b,c)
+	cout<<Tong<<endl;
+	cout<<Hieu<<endl;
+	cout<<Nhan<<endl;
 }
