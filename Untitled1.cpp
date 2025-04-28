cf5d28e5dd0f269fa5987e6ac9b970842721e4a8
zhangg183 <phanhieu183203@gmail.com>
2025-04-28 20:43:53 +0700

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -3,8 +3,13 @@ using namespace std;
 int tong(int a,int b, int c){
 	return a+b+c;
 }
+int hieu(int a,int b, int c){
+	return a-b-c;
+}
 int main(){
 	int a=7,b=8,c=9;
 	int Tong = tong(a,b,c);
+	int Hieu = hieu(a,b,c);
 	cout<<Tong<<endl;
+	cout<<HIeu<<endl;
 }
