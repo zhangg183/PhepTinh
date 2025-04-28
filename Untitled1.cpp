a7b15ae8602a1de1ef60c6a262913c2f504f7db3
zhangg183 <phanhieu183203@gmail.com>
2025-04-28 20:47:59 +0700

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -5,7 +5,8 @@ int main(){
 	int a=7,b=8,c=9;
 	int Tong = tong(a,b,c);
 	int Hieu = hieu(a,b,c);
-	int nhan = nhan(a,b,c)
+	int Nhan = nhan(a,b,c)
 	cout<<Tong<<endl;
-	cout<<HIeu<<endl;
+	cout<<Hieu<<endl;
+	cout<<Nhan<<endl;
 }
